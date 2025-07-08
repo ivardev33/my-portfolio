5df65e35d5b6355890c145eca70bdc421cc16eee
Ivar <ivarrobot89@gmail.com>
2025-07-08 19:29:10 +0200

    cambios iconos

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { motion } from 'framer-motion'
+//import { motion } from 'framer-motion'
 import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaGitAlt } from 'react-icons/fa'
 
 import { SiNextdotjs, SiTailwindcss, SiFirebase, SiKotlin, SiJavascript, SiJetpackcompose,SiMysql } from 'react-icons/si'
