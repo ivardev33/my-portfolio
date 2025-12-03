3651d26b5bd9ecd3ab07d5b05783eb47483231a3
Ivar <143705346+ivardev33@users.noreply.github.com>
2025-12-03 17:43:24 +0100

    Arreglos cv

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,28 +1,28 @@
-export default function Contact(){
+export default function Contact() {
 
 
-return(
+  return (
 
 
 
-<section id="contact" className=" animate-on-scroll min-h-screen flex flex-col items-center justify-center p-8 bg-black-100">
-        <h2 className="text-3xl font-bold mb-6">Contacto</h2>
+    <section id="contact" className=" animate-on-scroll min-h-screen flex flex-col items-center justify-center p-8 bg-black-100">
+      <h2 className="text-3xl font-bold mb-6">Contacto</h2>
 
-        <p className="text-gray-700 mb-4">
-          Puedes escribirme a:{' '}
-          <a href="mailto:ivan.cano.p@outlook.com" className="text-blue-500 underline">
-            ivan.cano.p@outlook.com
-          </a>
-        </p>
-
-        <a
-          href="/cv/Ivan-CV.pdf"
-          download
-          className="mt-4 inline-block px-6 py-3 bg-black text-white border border-gray-300 rounded-lg hover:bg-gray-800 transition"
-        >
-          Descargar CV
+      <p className="text-gray-700 mb-4">
+        Puedes escribirme a:{' '}
+        <a href="mailto:ivan.cano.p@outlook.com" className="text-blue-500 underline">
+          ivan.cano.p@outlook.com
         </a>
-      </section>
+      </p>
+
+      <a
+        href="/cv/Curriculum_Ivan_cano.pdf"
+        download
+        className="mt-4 inline-block px-6 py-3 bg-black text-white border border-gray-300 rounded-lg hover:bg-gray-800 transition"
+      >
+        Descargar CV
+      </a>
+    </section>
 
 
 
@@ -30,7 +30,7 @@ return(
 
 
 
-)
+  )
 
 
 
